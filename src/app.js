b9dee8c4aaf186ab0e2696aa169d2632711952c4
aakritrajput <aakritrajput87@gmail.com>
2025-01-15 11:09:35 +0530

    fix cors configuration; update origin to specific frontend URL for improved security

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from "cors"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: 'https://blog-app-frontend-sooty.vercel.app',
     credentials: true
 }))
 app.use(express.json({limit: "50kb"}))
